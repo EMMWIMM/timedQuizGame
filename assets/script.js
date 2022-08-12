ee4a4556a54e725d58bf26ccf3d7802f49d33656
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-08-11 18:39:02 -0600

    removed duplicate counter

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ var question= document.querySelector(".question");
 var end= document.querySelector(".end");
 var scoreContainer= document.querySelector(".highscores");
 var quiz = document.querySelector(".quiz");
-var timeLeft = 10;
+var timeLeft = 100;
 var points = document.querySelector(".points")
 var pointsEarned = document.querySelector(".pointsEarned")
 var madeIt = document.querySelector(".madeIt")
@@ -193,8 +193,6 @@ function initialsSubmit(){
     end.style.display = "none";
     madeIt.style.display = "block";
 
-
-      return;
   }
   pointsEarned.textContent = "points:" + score + "/10";
   name.textContent = initialIN;
@@ -218,38 +216,13 @@ if(runningQindex>= lastQIndex || timeLeft == 0){
   name.innerHTML= name;
   score.innerHTML = score
 }
+
 }
 
 
 
 
-//game timer
-function countdown() {
-  // console.log("countdown() started");
 
-
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `endGame'
-      endGame();
-    }
-  }, 1000);
-}
 
 
 // function smallCount() {
